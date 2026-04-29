5e045788ddbb8f9134a35bc614fd3f3204f595c4
JS-Patel12 <jaypatel122010@gmail.com>
2026-04-29 22:05:50 +0530

    The MidnightC

---
+++ BMI/Calculating BMI(Ladder).c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+main()
+{
+	int weight;
+	float height;
+	
+	printf("Enter your weight : ");
+	scanf("%d",&weight);
+	printf("Enter your height : ");
+	scanf("%f",&height);
+	
+	float BMI = weight/(height*height)*703;
+	
+	if(BMI < 18.5)
+	{
+		printf("You are Underweight.");
+	}
+	else if(BMI >= 18.5 && BMI <= 24.9)
+	{
+		printf("You have an Healthy Weight.");
+	}
+	else if(BMI >= 25.0 && BMI <= 29.9)
+	{
+		printf("You are Overweight.");
+	}
+	else
+	{
+		printf("You are Obese.");
+	}
+}
