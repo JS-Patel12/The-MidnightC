038e09d8249259766d689214b174aad252bfd00b
JS-Patel12 <jaypatel122010@gmail.com>
2026-06-08 22:31:41 +0530

    The Midnight C

---
+++ Simple Calculator.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+main()
+{
+    int a,b;
+    char c;
+    printf("Enter the first value : ");
+    scanf("%d",&a);
+    printf("Enter the symbol : ");
+    scanf("%d",&c);
+    printf("Enter the second value : ");
+    scanf("%d",&b);
+
+    if (c = /)
+    {
+        printf("%d",a/b);
+    }
+    else if (c = *)
+    {
+        printf("%d",a*b);
+    }
+    else if (c = +)
+    {
+        printf("%d",a+b);
+    }
+    else
+    {
+        printf("%d",a-b);
+    }
+}
